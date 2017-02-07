5caed0623ece96f1e99852b5b8385f79ee78bfe2
Kevin Keller <flygeneticist@gmail.com>
2017-02-07 15:38:17 +0800

    adds config settings file load for app setup

--- src/public/index.php
+++ src/public/index.php
@@ -2,9 +2,10 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+require '../config.php';
 require '../vendor/autoload.php';
 
-$app = new \Slim\App;
+$app = new \Slim\App(["settings" => $config]);
 
 $app->get('/hello/{name}', function (Request $req, Response $res) {
     $name = $req->getAttribute('name');
